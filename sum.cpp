947168e2cc75cc78c8f706a2ca7dd9886aeae9ae
tirthvarmora191-beep <tirthvarmora191@gmail.com>
2026-08-17 07:23:49 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int addnumbers(int first_param,int second_param){
+    int result = first_param + second_param;
+    return result;    
+}
+int main() {
+    /*int first_numbers {1};
+    int second_numbers {13};
+    cout << "first number: " << first_numbers << '\n';
+    cout << "second number: " << second_numbers << '\n';
+    int sum = first_numbers + second_numbers;
+    cout << "sum: " << sum << '\n';
+    sum = addnumbers(10, 2);
+    cout << "sum(10,2): " << sum << '\n';
+    return 0; */
+    
+}

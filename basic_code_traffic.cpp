e7839130f0ffb2218d5d60c9f7d3fd3251f79501
tirthvarmora191-beep <tirthvarmora191@gmail.com>
2026-08-18 19:55:19 +0530

    Add files via upload

---
+++ basic_code_traffic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool RedLight {false};
+    bool GreenLight {true};
+    if (RedLight==true){
+        cout<<" stop !!!";
+    }else{
+        cout<<"goooo ";
+    }
+      
+
+}

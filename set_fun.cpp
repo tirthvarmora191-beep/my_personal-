45b57dfa06fb3161aef5cb1c98d58c11dd231065
tirthvarmora191-beep <tirthvarmora191@gmail.com>
2026-08-20 22:20:10 +0530

    Add files via upload

---
+++ set_fun.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+//#include<iomanip>
+using namespace std;
+int main(){
+    /*cout<<setfill('-');
+    cout<<setw(10)<<" tirth"<<setw(10)<<"varmora";
+    cout<<setw(10)<<" jay"<<setw(10)<<"mataji";
+    float a=3.12345678912345;
+    cout<<setprecision(5);
+    cout<<setprecision(10);
+    cout<<setprecision(8);
+    cout<<a;*/
+      
+}

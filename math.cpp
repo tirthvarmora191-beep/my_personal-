ee59249d4e923d1ba6ca1b0219487994d18af32c
tirthvarmora191-beep <tirthvarmora191@gmail.com>
2026-08-21 22:40:09 +0530

    Add files via upload

---
+++ math.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    cout<<pow(2,3);
+    cout<<("\n");
+    cout<<log10(1000); //int value
+    cout<<("\n");
+    cout<<log10f(28383);// float value
+    cout<<("\n");
+    cout<<sqrt(100);
+    cout<<("\n");
+    cout<<round(2.3);
+    cout<<("\n");
+    cout<<round(34.4323);
+      
+}
